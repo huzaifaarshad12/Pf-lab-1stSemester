774d194c39f60a0ad1e3bc3605718fe185c93994
huzaifaarshad12 <144416664+huzaifaarshad12@users.noreply.github.com>
2023-10-02 16:00:47 +0500

    Add files via upload

---
+++ week5/task3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+main(){
+	float num;
+	cout<<"Enter a number: ";
+	cin>> num;
+
+	cout<<"The square root of "<<num<<" is: "<<sqrt(num);
+
+}
