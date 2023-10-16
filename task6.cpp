d18f4621e52527fe76c249fee005a9f97d65042d
huzaifaarshad12 <144416664+huzaifaarshad12@users.noreply.github.com>
2023-10-16 03:51:53 -0700

    Add files via upload

---
+++ task6.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int GCD(int,int);
+int LCM(int,int);
+main(){
+    int num1,num2;
+    cout<<"Enter the first number: ";
+    cin>>num1;
+
+    cout<<"Enter the second number: ";
+    cin>>num2;
+
+    int output = GCD(num1,num2);
+    int result = LCM(num1,num2);
+    cout<<"GCD: "<<output;
+    cout<<"LCM: "<<result;
+    
+}
+int LCM(int,int){
+
+
+
+
+
+
+
+
+}
+int GCD(int,int){
+
+
+
+
+
+
+
+
+}
