845a8bec51af58039eedb2038553b4eb52e2b600
Huziafa Arshad <144416664+huzaifaarshad12@users.noreply.github.com>
2024-08-23 20:04:17 +0500

    task6.cpp

--- week3/task6.cpp
+++ week3/task6.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 using namespace std;
-main(){
-long double mb;
-long double bit;
-cout<<"Enter the size in megabytes (MB): ";
-cin>>mb;
-bit=mb*1024*1024*8;
-cout<<mb<<" MB is equivalent to "<<bit<<" bits.";
+main()
+{
+  long double mb;
+  long double bit;
+  
+  cout<<"Enter the size in megabytes (MB): ";
+  cin>>mb;
+  
+  bit=mb*1024*1024*8;
+  cout<<mb<<" MB is equivalent to "<<bit<<" bits.";
 }
 
