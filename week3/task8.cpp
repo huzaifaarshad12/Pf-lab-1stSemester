e32a0bbdb755306e56b16b1cb052e1826d034fc8
Huziafa Arshad <144416664+huzaifaarshad12@users.noreply.github.com>
2024-08-23 20:05:46 +0500

    task8.cpp

--- week3/task8.cpp
+++ week3/task8.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
-main(){
-int v;
-float i,p;
-cout<<"Enter voltage (in volts): ";
-cin>>v;
-cout<<"Enter current (in amperes): ";
-cin>>i;
-p=v*i;
-cout<<"The power is "<<p<<" watts.";
+main()
+{
+  int v;
+  float i,p;
+  
+  cout<<"Enter voltage (in volts): ";
+  cin>>v;
+  
+  cout<<"Enter current (in amperes): ";
+  cin>>i;
+  p=v*i;
+  cout<<"The power is "<<p<<" watts.";
 }
