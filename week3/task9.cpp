eb57c1f3db79c2dcdba56e46eaa10a79c90e9856
Huziafa Arshad <144416664+huzaifaarshad12@users.noreply.github.com>
2024-08-23 20:06:37 +0500

    task9.cpp

--- week3/task9.cpp
+++ week3/task9.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
-main(){
-int y,d;
-cout<<"Enter your age in years: ";
-cin>>y;
-d=y*365;
-cout<<"Your age in days is approximately "<<d<<" days.";
+main()
+{
+  int y,d;
+  
+  cout<<"Enter your age in years: ";
+  cin>>y;
+  
+  d=y*365;
+  cout<<"Your age in days is approximately "<<d<<" days.";
 }
