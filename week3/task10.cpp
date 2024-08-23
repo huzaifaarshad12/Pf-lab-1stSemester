cd517b8917197d588fb7891e3820e17a1bc591d2
Huziafa Arshad <144416664+huzaifaarshad12@users.noreply.github.com>
2024-08-23 20:00:14 +0500

    task10.cpp

--- week3/task10.cpp
+++ week3/task10.cpp
@@ -1,18 +1,24 @@
 #include<iostream>
 using namespace std;
-main(){
+main()
+{
 	string name;
 	int w,d,l,points;
+	
 	cout<<"Enter the name of the cricket team: ";
 	cin>>name;
+	
 	cout<<"Enter the number of wins: ";
 	cin>>w;
+	
 	w=w*3;
 	cout<<"Enter the number of draws: ";
 	cin>>d;
+	
 	d=d*1;
 	cout<<"Enter the number of losses: ";
 	cin>>l;
+	
 	l=l*0;
 	points=w+d+l;
 	cout<<name<< " has obtained "<<points<<" points in the Asia Cup tournament.";
