9fdb37ab626018d4f637374dc0b49758f1fdf3f1
Huziafa Arshad <144416664+huzaifaarshad12@users.noreply.github.com>
2024-08-23 20:04:59 +0500

    task7.cpp

--- week3/task7.cpp
+++ week3/task7.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
-main(){
-int second,hours;
-cout<<"Enter the number of hours: ";
-cin>>hours;
-second=hours*60*60;
-cout<<hours<<" hours is equivalent to "<<second<<" seconds.";
+main()
+{
+  int second,hours;
+  cout<<"Enter the number of hours: ";
+  cin>>hours;
+  
+  second=hours*60*60;
+  cout<<hours<<" hours is equivalent to "<<second<<" seconds.";
 }
